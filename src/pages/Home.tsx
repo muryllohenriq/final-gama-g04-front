e8aeb490c5bc105259045933b0d9fd0ac98231a5
LuanaGSouza21 <63968764+lusouzarego@users.noreply.github.com>
2022-11-10 20:51:35 +0100

    start branch luana

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
 import Header from "../components/Header";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import "./Home.css";
 
-function Home() {
-    return (
-        <>
-            <Header />
-            <h1>Home</h1>
-        </>
-    )
+function Navbar() {
+  return (
+    <>
+      <NavbarBs>
+        <Container>
+          <Nav>
+            <Button variant="secondary" href="#bebidas">
+              Bebidas
+            </Button>{" "}
+            <Button id="styleButtons" href="#entradas">
+              Entradas
+            </Button>{" "}
+            <Button id="styleButtons" href="#saladas">
+              Saladas{" "}
+            </Button>
+            <Button id="styleButtons" href="#hamburgueres">
+              Hambúrgueres{" "}
+            </Button>
+          </Nav>
+        </Container>
+      </NavbarBs>
+    </>
+  );
 }
 
-export default Home;
+export default Navbar;
