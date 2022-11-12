8c51b0c39da5884d6eedf6bbd44f0cceb9bc0aad
Karolinegaia <drakarolinegaia@gmail.com>
2022-11-11 22:24:53 -0300

    Creating new branch

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -18,22 +18,6 @@ export function Card() {
     )
 }
 
-export function CardDatails() {
-  return (
-    <CardStyle>
-      <img className="image" src="/src/assets/Rectangle 4.png" alt="" />
-      <div className="info">
-        <div className="badge-div">
-          <Badge id="1"/>
-          <Badge id="2"/>
-        </div>
-      <strong>Guioza de legumes</strong>
-      <p className="description">Texto de descrição. Texto de descrição. Texto de de...</p>
-      <p className="price">R$ 00,00</p>
-      </div>
-    </CardStyle>
-    )
-}
 
 
 
