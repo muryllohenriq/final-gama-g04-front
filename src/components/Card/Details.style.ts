105940ddb6bb8dad455dbb9a2cc29fb9fc4a7f14
Alyce Monteiro <mariamariamaria171@hotmail.com>
2022-11-14 15:42:00 -0300

    Editing Details files

--- src/components/Card/Details.style.ts
+++ src/components/Card/Details.style.ts
@@ -1,21 +1,36 @@
 import styled from 'styled-components'
 
-export const DetailsStyle = styled.section `
-   
+export const DetailsStyle = styled.section`
+  .info {
     background: none;
     flex-direction: column;
     width: 300px;
     height: 150px;
     border-radius: 16px;
     display: flex;
-    align-items: center;   
     margin-top: 4px;
-
-    .image{
-      margin-left: 25px;
-      width: 250px;
-      height: 150px;
-    }
-`
-
-
+  }
+  .food-name {
+    font-size: 24px;
+    margin-left: 2rem;
+  }
+  .text {
+    margin-top: 2rem;
+    margin-left: 2rem;
+  }
+  .image {
+    margin-top: 2rem;
+    margin-left: 2rem;
+    width: 300px;
+    height: 150px;
+  }
+  .badges {
+    margin-top: 1rem;
+    padding-left: 15rem;
+    margin-bottom: 1rem;
+  }
+  .price{
+    margin-top: 1rem;
+    margin-left: 14rem;
+  }
+`;
