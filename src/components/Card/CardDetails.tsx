b0bbbe6e49295e427571d95f940cbedb3dc5e0e3
Bruno Bastos <mairla136@gmail.com>
2022-11-14 14:01:33 -0300

    Creating card details

---
+++ src/components/Card/CardDetails.tsx
@@ -0,0 +1,23 @@
+import { CardStyle } from "./Card.style";
+import {Badge} from "../Badge/Badge"
+
+type CardProps = {
+  cardClass?: string;
+}
+
+export function CardDetails(props: CardProps) {
+    return (
+      <CardDetailsStyle className={props.cardClass}>
+        <img className="image" src="/src/assets/Rectangle 4.png" alt="" />
+        <div className="info">
+          <div className="badge-div">
+            <Badge id="1"/>
+            <Badge id="2"/>
+          </div>
+        <strong>Guioza de legumes</strong>
+        <p className="description">Texto de descrição. Texto de descrição. Texto de de...</p>
+        <p className="price">R$ 00,00</p>
+        </div>
+      </CardDetailsStyle>
+      )
+  }
