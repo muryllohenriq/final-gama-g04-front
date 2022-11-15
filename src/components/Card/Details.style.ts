8abfc5b5cc696010136faae66a499b05d57c82e8
Alyce Monteiro <mariamariamaria171@hotmail.com>
2022-11-15 16:20:38 -0300

    Adding a few more updates

--- src/components/Card/Details.style.ts
+++ src/components/Card/Details.style.ts
@@ -12,39 +12,62 @@ export const DetailsStyle = styled.section`
   }
   .food-name {
     font-size: 24px;
-    margin-top: 2rem;
-    padding-left: 2rem;
-    width: 60%;
+    font-weight: 600;
+    margin-top: 0.25rem;
+    margin-bottom: 0rem;
+    margin-left: 2.5rem;
+    width: 65%;
   }
   .icon {
-    
+    width: 18px;
+    height: 13px;
   }
-  .description {
-    flex-direction: column;
-    align-items: flex-start;
-    padding-left: 0.25rem;
-    margin-top: 2rem;
-  }
-  .text {
-    padding-left: 2rem;
-    margin-top: 3rem;
-    box-sizing: border-box;
+  .people {
+    font-size: 14px;
+    font-weight: 300;
+    color: #676767;
+    margin-left: 2.5rem;
     width: 60%;
   }
-  .image {
-    margin-top: 1rem;
-    margin-left: 2rem;
-    width: 300px;
-    height: 150px;
+  .description {
+    font-size: 16px;
+    font-weight: 400;
+    color: #000000;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-top: 1.25rem;
+    margin-right: 2.5rem;
   }
-  .badges {
+  .text {
+    margin-left: 2.5rem;
+    margin-top: 1rem;
+    width: 75%;
+  }
+  .image {
     display: flex;
     justify-content: flex-end;
-    margin-top: 2rem;
-    margin-right: 6.5rem;
+    margin-top: 1rem;
+    margin-left: 2.5rem;
+    margin-right: 2.5rem;
+    width: 310px;
+    height: 176px;
+  }
+  .badges {
+    font-size: 12px;
+    font-weight: 500;
+    color: #000000;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 0.5rem;
+    margin-right: 2.5rem;
   }
   .price {
-    margin-top: 1rem;
-    margin-left: 14rem;
+    font-size: 16px;
+    font-weight: 400;
+    color: #000000;
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 2.5rem;
+    padding-top: 0.75rem;
   }
-`;
+`
--- src/components/Card/Details.tsx
+++ src/components/Card/Details.tsx
@@ -9,23 +9,25 @@ type CardProps = {
 export function CardDetails(props: CardProps) {
     return (
       <DetailsStyle className={props.cardClass}>
-        <img className="image" src="/src/assets/Rectangle 4.png" alt="" />
-        <div className="info">
-          <div className="badges">
-            <Badge id="1" text="details" />
-            <Badge id="2" text="details" />
+        <div className="container">
+          <img className="image" src="/src/assets/Rectangle 4.png" alt="" />
+          <div className="info">
+            <div className="badges">
+              <Badge id="1" text="details" />
+              <Badge id="2" text="details" />
+            </div>
+            <div className="description">
+              <p className="food-name">Guioza de legumes</p>
+              <p className="people">
+                <img src={Icon} className="icon" alt="icone de pessoas"></img>
+                Serve até 3 pessoas
+              </p>
+              <p className="text">
+                Texto de descrição. Texto de descrição. Texto de descriç...
+              </p>
+            </div>
+            <p className="price">R$ 00,00</p>
           </div>
-          <div className="description">
-            <strong className="food-name">Guioza de legumes</strong>
-            <p className="people">
-              <img src={Icon} className="icon" alt="icone de pessoas"></img>
-              Serve até 3 pessoas
-            </p>
-            <p className="text">
-              Texto de descrição. Texto de descrição. Texto de descrição
-            </p>
-          </div>
-          <p className="price">R$ 00,00</p>
         </div>
       </DetailsStyle>
     );
