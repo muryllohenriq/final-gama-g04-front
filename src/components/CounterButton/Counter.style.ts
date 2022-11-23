53b61db4dcadeda3a9c3f458d6057304d7291d1b
jvdalaglio <jvdalaglio@gmail.com>
2022-11-22 23:17:44 -0300

    Transforming CounterButton to component

---
+++ src/components/CounterButton/Counter.style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const CounterStyle = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+  background-color: #DCDCDC;
+  width: 80px;
+  border-radius: 8px;
+  height: 30px;
+  margin: -10px 5px 20px 0;
+
+
+
+p {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 110%;
+  color: #000000;
+  margin: 0;
+}
+`
