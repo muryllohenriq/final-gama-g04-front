896cc852850e290bf1b69a4e5e536e11721cdca3
unknown <muryllo.henrique@hotmail.com>
2022-11-25 19:06:38 -0300

    Creating bill page

--- src/pages/Bill.tsx
+++ src/pages/Bill.tsx
@@ -1,10 +1,52 @@
+import { useEffect, useState } from "react";
 import { ControlHeader } from "../components/ControlHeader/Control";
 import { MenuBottom } from "../components/MenuBottom/MenuBottom";
+import api from "../services/api";
 
-export function Bill() {
+type OrderProps = {
+  order: {
+        name: string;
+        comment: string;
+    },
+}
+
+type TableProps = {
+    id: string;
+    idTable: string;
+    command: number;
+    order: OrderProps[];
+}
+
+export function Bill(props: TableProps) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [orderList, setOrderList] = useState<TableProps[]>([])
+
+  async function getOrderData() {
+    const {data} = await api.get("tables/1")
+    
+    setOrderList(data.order);
+    setIsLoading(false)
+  }
+
+  useEffect(() => { 
+    getOrderData()
+  }, [])
+
+  if (isLoading) {
+    return null;
+  }
+
   return(
     <>
     <ControlHeader text="Fechar conta" name="Conta" />
+    <div>
+      {orderList.map((data, index) => (
+              <div
+                key={index}
+                id={data.idTable}
+              />
+            ))}
+    </div>
     <MenuBottom />
     </>
   )
